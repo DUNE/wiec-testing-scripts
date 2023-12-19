c1b4ff427feec4a14685678268eb15c51fab1451
Eric <eraguzin@bnl.gov>
2023-12-19 17:28:54 -0500

    Got a main sequence skeleton running. Getting it to work forced me to fix some bugs in each of the instruments' individual APIs

--- dune_hv_crate_test.py
+++ dune_hv_crate_test.py
@@ -2,6 +2,7 @@ import pyvisa
 import sys
 import json
 import os
+import time
 from keysight_daq970a import Keysight970A
 from rigol_dp832a import RigolDP832A
 from caen_r8033dm_wrapper import CAENR8033DM_WRAPPER
@@ -14,31 +15,105 @@ class LDOmeasure:
             self.json_data = json.load(jsonfile)
         self.rm = pyvisa.ResourceManager('@py')
 
-        c = CAENR8033DM_WRAPPER(self.json_data)
-        #sys.exit("We have finished the Caen")
+        #Initialize all instruments first so that you don't waste time with input if something is not connected
+        self.c = CAENR8033DM_WRAPPER(self.json_data)
+        self.k = Keysight970A(self.rm, self.json_data)
 
-        k = Keysight970A(self.rm, self.json_data)
-        k.set_relay(24, 69)
-        k.initialize_resistance()
-        print(k.measure_resistance())
-        k.initialize_fan()
-        print(k.measure_fan())
-        k.initialize_fan()
-        print(k.initialize_rtd())
-        k.heaters_on()
+        #Since there are 2 Rigols, set them up here so they know what channels they have
+        self.r0 = RigolDP832A(self.rm, self.json_data, 0)
+        self.r0.setup_fan()
+        self.r0.setup_heater_supply()
+        self.r0.setup_heater_switch()
 
-        r = RigolDP832A(self.rm, self.json_data)
-        r.initialize()
+        self.r1 = RigolDP832A(self.rm, self.json_data, 1)
+        self.r1.setup_hvpullup()
+        self.r1.setup_fanread()
+        self.sequence()
+
+    def sequence(self):
+        #Fan test
+        self.k.initialize_fan()
+        self.r0.power("ON", "fan")
+        self.r1.power("ON", "fanread")
+        print(f"{self.prefix} --> Fans turned on, waiting {float(self.json_data['fan_wait'])} seconds for the fans to reach steady state...")
+        time.sleep(float(self.json_data['fan_wait']))
+        fan_voltage = self.r0.get_voltage("fan")
+        fan_current = self.r0.get_current("fan")
+        fanread_voltage = self.r1.get_voltage("fanread")
+        fanread_current = self.r1.get_current("fanread")
+        fan_read_signal = self.k.measure_fan()
+        self.r0.power("OFF", "fan")
+        print(f"{self.prefix} --> Fans turned off")
+        print(f"{self.prefix} --> Fan power supply was {fan_voltage}V and {fan_current}A")
+        print(f"{self.prefix} --> Read signal for each fan was {fan_read_signal}")
+        print(f"{self.prefix} --> Fan read pullup supply was {fanread_voltage}V and {fanread_current}A")
+
+        #Heater test
+        #First measure resistance of heating element with no power connected
+        self.k.initialize_resistance()
+        heater_resistance = self.k.measure_resistance()
+        print(f"{self.prefix} --> Heating element resistances are {fan_read_signal}")
+
+        #Then prepare RTD and switch relay to connect power
+        self.k.initialize_rtd()
+        temp1 = self.k.measure_rtd()
+        self.r0.power("ON", "heat_supply")
+        self.r0.power("ON", "heat_switch")
+        print(f"{self.prefix} --> Heat turned on, waiting {float(self.json_data['heat_wait'])} seconds for the sensors to heat up...")
+        time.sleep(float(self.json_data['heat_wait']))
+        supply_voltage = self.r0.get_voltage("heat_supply")
+        supply_current = self.r0.get_current("heat_supply")
+        switch_voltage = self.r0.get_voltage("heat_switch")
+        switch_current = self.r0.get_current("heat_switch")
+        temp2 = self.k.measure_rtd()
+
+        self.r0.power("OFF", "heat_supply")
+        self.r0.power("OFF", "heat_switch")
+        print(f"{self.prefix} --> Heat turned off")
+        print(f"{self.prefix} --> Heat power supply was {supply_voltage}V and {supply_current}A")
+        print(f"{self.prefix} --> Heat power switch was {switch_voltage}V and {switch_current}A")
+        print(f"{self.prefix} --> Original temperatures were {temp1}")
+        print(f"{self.prefix} --> Temperatures after {float(self.json_data['heat_wait'])} seconds were {temp2}")
+
+        #HV Leakage Test
+        #Turn on HV
+        hv_results = {}
+        self.r1.power("ON", "hvpullup")
+        for i in range(8):
+            #Do the positive voltage with open termination
+            self.k.set_relay(0 << i, 0)
+            self.c.turn_on(i)
+            hv_results[i] = {"P,NT,V" : self.c.get_voltage(i)}
+            hv_results[i]["P,NT,C"] = self.c.get_current(i)
+
+            #Turn on 10k termination
+            self.k.set_relay(0 << i, 1 << i)
+            print(f"{self.prefix} --> HV termination switched, waiting {float(self.json_data['hv_termination_wait'])} seconds...")
+            time.sleep(float(self.json_data['hv_termination_wait']))
+
+            hv_results[i]["P,10k,V"] = self.c.get_voltage(i)
+            hv_results[i]["P,10k,C"] = self.c.get_current(i)
+            self.c.turn_off(i)
+
+            #Turn on negative voltage
+            self.k.set_relay(1 << i, 0)
+            self.c.turn_on(i+8)
+            hv_results[i]["N,NT,V"] = self.c.get_voltage(i)
+            hv_results[i]["N,NT,C"] = self.c.get_current(i)
+
+            #Turn on 10k termination
+            self.k.set_relay(1 << i, 1 << i)
+            print(f"{self.prefix} --> HV termination switched, waiting {float(self.json_data['hv_termination_wait'])} seconds...")
+            time.sleep(float(self.json_data['hv_termination_wait']))
+
+            hv_results[i]["N,10k,V"] = self.c.get_voltage(i)
+            hv_results[i]["N,10k,C"] = self.c.get_current(i)
+            self.c.turn_off(i+8)
+
+            print(f"{self.prefix} --> Channel {i} HV results are {hv_results[i]}")
 
 
-        r.get_current("fan")
-        print(r.get_voltage("fan"))
-        print(r.check_overcurr_protection("fan"))
-        r.power("ON", "fan")
-
-        self.name = name
-        self.begin_measurement()
-
+        print(f"{self.prefix} --> Test complete")
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
--- rigol_dp832a.py
+++ rigol_dp832a.py
@@ -6,27 +6,67 @@ Created on Wed Sept 25 10:51:58 2023
 """
 
 class RigolDP832A:
-    def __init__(self, rm, json_data):
+    def __init__(self, rm, json_data, index):
         self.prefix = "Rigol DP832A"
         self.json_data = json_data
-        self.rigol = rm.open_resource(self.json_data['rigol832a'])
+        self.rigol = rm.open_resource(self.json_data[f'rigol832a{index}'])
         print(f"{self.prefix} --> Connected to {self.rigol.query('*IDN?')}")
         self.rigol.write("*RST")
+        self.channels = []
+        self.index = index
+        self.channel_num = 3
+
+    def setup_fan(self):
+        self.rigol.write(f"SOURce{int(self.json_data['rigol832a_fan_ch']) - (self.channel_num * self.index)}:VOLTage:LEVel:IMMediate:AMPLitude {self.json_data['rigol832a_fan_voltage']}")
+        self.rigol.write(f"SOURce{int(self.json_data['rigol832a_fan_ch']) - (self.channel_num * self.index)}:CURRent:LEVel:IMMediate:AMPLitude {self.json_data['rigol832a_fan_current']}")
+        self.rigol.write(f"SOURce{int(self.json_data['rigol832a_fan_ch']) - (self.channel_num * self.index)}:CURRent:PROTection:LEVel {self.json_data['rigol832a_fan_overcurrent']}")
+        self.rigol.write(f"SOURce{int(self.json_data['rigol832a_fan_ch']) - (self.channel_num * self.index)}:CURRent:PROTection:STATe {self.json_data['rigol832a_fan_overcurrent_en']}")
+        self.channels.append("fan")
+
+    def setup_heater_supply(self):
+        self.rigol.write(f"SOURce{int(self.json_data['rigol832a_heater_supply_ch']) - (self.channel_num * self.index)}:VOLTage:LEVel:IMMediate:AMPLitude {self.json_data['rigol832a_heater_supply_voltage']}")
+        self.rigol.write(f"SOURce{int(self.json_data['rigol832a_heater_supply_ch']) - (self.channel_num * self.index)}:CURRent:LEVel:IMMediate:AMPLitude {self.json_data['rigol832a_heater_supply_current']}")
+        self.rigol.write(f"SOURce{int(self.json_data['rigol832a_heater_supply_ch']) - (self.channel_num * self.index)}:CURRent:PROTection:LEVel {self.json_data['rigol832a_heater_supply_overcurrent']}")
+        self.rigol.write(f"SOURce{int(self.json_data['rigol832a_heater_supply_ch']) - (self.channel_num * self.index)}:CURRent:PROTection:STATe {self.json_data['rigol832a_heater_supply_overcurrent_en']}")
+        self.channels.append("heat_supply")
+
+    def setup_heater_switch(self):
+        self.rigol.write(f"SOURce{int(self.json_data['rigol832a_heater_switch_ch']) - (self.channel_num * self.index)}:VOLTage:LEVel:IMMediate:AMPLitude {self.json_data['rigol832a_heater_switch_voltage']}")
+        self.rigol.write(f"SOURce{int(self.json_data['rigol832a_heater_switch_ch']) - (self.channel_num * self.index)}:CURRent:LEVel:IMMediate:AMPLitude {self.json_data['rigol832a_heater_switch_current']}")
+        self.rigol.write(f"SOURce{int(self.json_data['rigol832a_heater_switch_ch']) - (self.channel_num * self.index)}:CURRent:PROTection:LEVel {self.json_data['rigol832a_heater_switch_overcurrent']}")
+        self.rigol.write(f"SOURce{int(self.json_data['rigol832a_heater_switch_ch']) - (self.channel_num * self.index)}:CURRent:PROTection:STATe {self.json_data['rigol832a_heater_switch_overcurrent_en']}")
+        self.channels.append("heat_switch")
+
+    def setup_hvpullup(self):
+        self.rigol.write(f"SOURce{int(self.json_data['rigol832a_hvpullup_ch']) - (self.channel_num * self.index)}:VOLTage:LEVel:IMMediate:AMPLitude {self.json_data['rigol832a_hvpullup_voltage']}")
+        self.rigol.write(f"SOURce{int(self.json_data['rigol832a_hvpullup_ch']) - (self.channel_num * self.index)}:CURRent:LEVel:IMMediate:AMPLitude {self.json_data['rigol832a_hvpullup_current']}")
+        self.rigol.write(f"SOURce{int(self.json_data['rigol832a_hvpullup_ch']) - (self.channel_num * self.index)}:CURRent:PROTection:LEVel {self.json_data['rigol832a_hvpullup_overcurrent']}")
+        self.rigol.write(f"SOURce{int(self.json_data['rigol832a_hvpullup_ch']) - (self.channel_num * self.index)}:CURRent:PROTection:STATe {self.json_data['rigol832a_hvpullup_overcurrent_en']}")
+        self.channels.append("hvpullup")
+
+    def setup_fanread(self):
+        self.rigol.write(f"SOURce{int(self.json_data['rigol832a_fanread_ch']) - (self.channel_num * self.index)}:VOLTage:LEVel:IMMediate:AMPLitude {self.json_data['rigol832a_fanread_voltage']}")
+        self.rigol.write(f"SOURce{int(self.json_data['rigol832a_fanread_ch']) - (self.channel_num * self.index)}:CURRent:LEVel:IMMediate:AMPLitude {self.json_data['rigol832a_fanread_current']}")
+        self.rigol.write(f"SOURce{int(self.json_data['rigol832a_fanread_ch']) - (self.channel_num * self.index)}:CURRent:PROTection:LEVel {self.json_data['rigol832a_fanread_overcurrent']}")
+        self.rigol.write(f"SOURce{int(self.json_data['rigol832a_fanread_ch']) - (self.channel_num * self.index)}:CURRent:PROTection:STATe {self.json_data['rigol832a_fanread_overcurrent_en']}")
+        self.channels.append("fanread")
 
     #Because I want to decouple the name of the channel with the actual number, this will need to be called almost every time
     def get_ch_with_name(self, ch):
-        if (ch == "fan"):
-            return self.json_data['rigol832a_fan_ch']
+        if (ch == "fan" and "fan" in self.channels):
+            return int(self.json_data['rigol832a_fan_ch']) - (self.channel_num * self.index)
+        elif (ch == "heat_supply" and "heat_supply" in self.channels):
+            return int(self.json_data['rigol832a_heater_supply_ch']) - (self.channel_num * self.index)
+        elif (ch == "heat_switch" and "heat_switch" in self.channels):
+            return int(self.json_data['rigol832a_heater_switch_ch']) - (self.channel_num * self.index)
+        elif (ch == "hvpullup" and "hvpullup" in self.channels):
+            return int(self.json_data['rigol832a_hvpullup_ch']) - (self.channel_num * self.index)
+        elif (ch == "fanread" and "fanread" in self.channels):
+            return int(self.json_data['rigol832a_fanread_ch']) - (self.channel_num * self.index)
         else:
             print(f"{self.prefix} --> WARNING: Did not understand channel type {ch}")
+            print(f"{self.prefix} --> WARNING: Channels initialized for Rigol {self.index} are {self.channels}")
             return 0
-
-    def initialize(self):
-        #Setup the fan channel
-        self.rigol.write(f"SOURce{self.json_data['rigol832a_fan_ch']}:VOLTage:LEVel:IMMediate:AMPLitude {self.json_data['rigol832a_fan_voltage']}")
-        self.rigol.write(f"SOURce{self.json_data['rigol832a_fan_ch']}:CURRent:LEVel:IMMediate:AMPLitude {self.json_data['rigol832a_fan_current']}")
-        self.rigol.write(f"SOURce{self.json_data['rigol832a_fan_ch']}:CURRent:PROTection:LEVel {self.json_data['rigol832a_fan_overcurrent']}")
-        self.rigol.write(f"SOURce{self.json_data['rigol832a_fan_ch']}:CURRent:PROTection:STATe {self.json_data['rigol832a_fan_overcurrent_en']}")
 
     def power(self, onoff, ch):
         if (onoff == "ON" or onoff == "OFF"):
@@ -40,13 +80,13 @@ class RigolDP832A:
     def get_current(self, ch):
         chan = self.get_ch_with_name(ch)
         if (chan != 0):
-            curr = self.rigol.query(f"SOURce{chan}:CURRent:LEVel:IMMediate:AMPLitude?")
+            curr = self.rigol.query(f"MEASure:CURRent:DC? CH{chan}")
             return float(curr)
 
     def get_voltage(self, ch):
         chan = self.get_ch_with_name(ch)
         if (chan != 0):
-            volt = self.rigol.query(f"SOURce{chan}:VOLTage:LEVel:IMMediate:AMPLitude?")
+            volt = self.rigol.query(f"MEASure:VOLTage:DC? CH{chan}")
             return float(volt)
 
     def check_overcurr_protection(self, ch):
