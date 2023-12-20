dfe6d5a7e500ec5c6f942187bb4f395d73e295b8
Eric <eraguzin@bnl.gov>
2023-12-20 18:05:39 -0500

    Spreadsheet and JSON output now check for pass/fail for each test

--- dune_hv_crate_test.py
+++ dune_hv_crate_test.py
@@ -36,7 +36,6 @@ class LDOmeasure:
         else:
             self.test_name = input("Input the test name:\n")
 
-
         self.rounding_factor = int(self.json_data["rounding_factor"])
         self.datastore = {}
         self.datastore['input_params'] = self.json_data
@@ -75,6 +74,10 @@ class LDOmeasure:
             top_style.border = openpyxl.styles.Border(left=bd, top=bd, right=bd, bottom=bd)
             self.wb.add_named_style(top_style)
 
+            self.fail_style = openpyxl.styles.NamedStyle(name="fail")
+            self.fail_style.font = openpyxl.styles.Font(color="FF0000", name='Calibri')
+            self.wb.add_named_style(self.fail_style)
+
             #Yes I'm using magic numbers, but I need to build the spreadsheet in a specific way, hopefully it's only done once
             self.ws.cell(row=2, column=1, value="Test Name").style = top_style
             self.ws.cell(row=2, column=2, value="Date").style = top_style
@@ -88,19 +91,19 @@ class LDOmeasure:
             self.ws.cell(row=2, column=8, value="Fan 3 RD").style = top_style
             self.ws.cell(row=2, column=9, value="Fan 4 RD").style = top_style
 
-            self.ws.cell(row=1, column=10, value="Heater Test").style = top_style
+            self.ws.cell(row=1, column=10, value=f"Heater Test - Resistance for each heating element and temperature rise after heating for {self.json_data['heat_wait']} seconds").style = top_style
             self.ws.merge_cells(start_row=1, start_column=10, end_row=1, end_column=17)
             self.ws.cell(row=2, column=10, value="TC1_Resistance").style = top_style
-            self.ws.cell(row=2, column=11, value="TC1_Temp_Rise").style = top_style
-            self.ws.cell(row=2, column=12, value="TC2_Resistance").style = top_style
-            self.ws.cell(row=2, column=13, value="TC2_Temp_Rise").style = top_style
-            self.ws.cell(row=2, column=14, value="TC3_Resistance").style = top_style
-            self.ws.cell(row=2, column=15, value="TC3_Temp_Rise").style = top_style
-            self.ws.cell(row=2, column=16, value="TC4_Resistance").style = top_style
+            self.ws.cell(row=2, column=11, value="TC2_Resistance").style = top_style
+            self.ws.cell(row=2, column=12, value="TC3_Resistance").style = top_style
+            self.ws.cell(row=2, column=13, value="TC4_Resistance").style = top_style
+            self.ws.cell(row=2, column=14, value="TC1_Temp_Rise").style = top_style
+            self.ws.cell(row=2, column=15, value="TC2_Temp_Rise").style = top_style
+            self.ws.cell(row=2, column=16, value="TC3_Temp_Rise").style = top_style
             self.ws.cell(row=2, column=17, value="TC4_Temp_Rise").style = top_style
 
             self.ws.cell(row=1, column=18, value="HV Test - Resistance for each configuration").style = top_style
-            self.ws.merge_cells(start_row=1, start_column=12, end_row=1, end_column=21+(7*4))
+            self.ws.merge_cells(start_row=1, start_column=18, end_row=1, end_column=21+(7*4))
             for i in range(8):
                 self.ws.cell(row=2, column=18+(i*4), value=f"Ch{i}+ Open").style = top_style
                 self.ws.cell(row=2, column=19+(i*4), value=f"Ch{i}+ 10k").style = top_style
@@ -114,7 +117,8 @@ class LDOmeasure:
             self.wb.save(self.path_to_spreadsheet)
             self.row = 3
 
-        self.ws.cell(row=self.row, column=1, value=self.test_name)
+
+        self.ws.cell(row=self.row, column=1, value=self.test_name).style = "fail"
         self.ws.cell(row=self.row, column=2, value=datetime.today().strftime('%m/%d/%Y'))
         self.ws.cell(row=self.row, column=3, value=datetime.today().strftime('%I:%M:%S %p'))
         self.wb.save(self.path_to_spreadsheet)
@@ -138,12 +142,31 @@ class LDOmeasure:
         print(f"{self.prefix} --> Read signal for each fan was {fan_read_signal}")
         print(f"{self.prefix} --> Fan read pullup supply was {fanread_voltage}V and {fanread_current}A")
 
-        self.ws.cell(row=self.row, column=4, value=fan_voltage)
-        self.ws.cell(row=self.row, column=5, value=fan_current)
-        self.ws.cell(row=self.row, column=6, value=round(fan_read_signal[1], self.rounding_factor))
-        self.ws.cell(row=self.row, column=7, value=round(fan_read_signal[2], self.rounding_factor))
-        self.ws.cell(row=self.row, column=8, value=round(fan_read_signal[3], self.rounding_factor))
-        self.ws.cell(row=self.row, column=9, value=round(fan_read_signal[4], self.rounding_factor))
+        fan_test = True
+        if ((fan_voltage < float(self.json_data["fan_voltage_max"])) and (fan_voltage > float(self.json_data["fan_voltage_min"]))):
+            self.ws.cell(row=self.row, column=4, value=fan_voltage)
+            self.datastore['Tests'] = {'fan_voltage_test' : "Pass"}
+        else:
+            self.ws.cell(row=self.row, column=4, value=fan_voltage).style = "fail"
+            self.datastore['Tests']['fan_voltage_test'] = "Fail"
+            fan_test = False
+
+        if ((fan_current < float(self.json_data["fan_current_max"])) and (fan_current > float(self.json_data["fan_current_min"]))):
+            self.ws.cell(row=self.row, column=5, value=fan_current)
+            self.datastore['Tests']['fan_current_test'] = "Pass"
+        else:
+            self.ws.cell(row=self.row, column=5, value=fan_current).style = "fail"
+            self.datastore['Tests']['fan_current_test'] = "Fail"
+            fan_test = False
+
+        for i in range(1,5):
+            if ((fan_read_signal[i] < float(self.json_data["fan_read_max"])) and (fan_read_signal[i] > float(self.json_data["fan_read_min"]))):
+                self.ws.cell(row=self.row, column=5+i, value=round(fan_read_signal[i], self.rounding_factor))
+                self.datastore['Tests'][f'fan_signal_test_{i}'] = "Pass"
+            else:
+                self.ws.cell(row=self.row, column=5+i, value=round(fan_read_signal[i], self.rounding_factor)).style = "fail"
+                self.datastore['Tests'][f'fan_signal_test_{i}'] = "Fail"
+                fan_test = False
 
         self.datastore['fan_voltage'] = fan_voltage
         self.datastore['fan_current'] = fan_current
@@ -157,10 +180,15 @@ class LDOmeasure:
         heater_resistance = self.k.measure_resistance()
         print(f"{self.prefix} --> Heating element resistances are {fan_read_signal}")
 
-        self.ws.cell(row=self.row, column=10, value=round(heater_resistance[1], self.rounding_factor))
-        self.ws.cell(row=self.row, column=11, value=round(heater_resistance[2], self.rounding_factor))
-        self.ws.cell(row=self.row, column=12, value=round(heater_resistance[3], self.rounding_factor))
-        self.ws.cell(row=self.row, column=13, value=round(heater_resistance[4], self.rounding_factor))
+        heat_test = True
+        for i in range(1,5):
+            if ((heater_resistance[i] < float(self.json_data["heating_element_max"])) and (heater_resistance[i] > float(self.json_data["heating_element_min"]))):
+                self.ws.cell(row=self.row, column=9+i, value=round(heater_resistance[i], self.rounding_factor))
+                self.datastore['Tests'][f'heating_element_test_{i}'] = "Pass"
+            else:
+                self.ws.cell(row=self.row, column=9+i, value=round(heater_resistance[i], self.rounding_factor)).style = "fail"
+                self.datastore['Tests'][f'heating_element_test_{i}'] = "Fail"
+                heat_test = False
 
         self.datastore['heater_resistance'] = heater_resistance
 
@@ -169,7 +197,7 @@ class LDOmeasure:
         temp1 = self.k.measure_rtd()
         self.r0.power("ON", "heat_supply")
         self.r0.power("ON", "heat_switch")
-        print(f"{self.prefix} --> Heat turned on, waiting {float(self.json_data['heat_wait'])} seconds for the sensors to heat up...")
+        print(f"{self.prefix} --> Heat turned on, waiting {self.json_data['heat_wait']} seconds for the sensors to heat up...")
         time.sleep(float(self.json_data['heat_wait']))
         supply_voltage = self.r0.get_voltage("heat_supply")
         supply_current = self.r0.get_current("heat_supply")
@@ -190,10 +218,14 @@ class LDOmeasure:
         print(f"{self.prefix} --> Original temperatures were {temp1}")
         print(f"{self.prefix} --> Temperatures after {float(self.json_data['heat_wait'])} seconds were {temp2}, a rise of {temp_rise}")
 
-        self.ws.cell(row=self.row, column=14, value=round(temp_rise[0], self.rounding_factor))
-        self.ws.cell(row=self.row, column=15, value=round(temp_rise[1], self.rounding_factor))
-        self.ws.cell(row=self.row, column=16, value=round(temp_rise[2], self.rounding_factor))
-        self.ws.cell(row=self.row, column=17, value=round(temp_rise[3], self.rounding_factor))
+        for i in range(4):
+            if ((temp_rise[0] < float(self.json_data["temp_increase_max"])) and (temp_rise[0] > float(self.json_data["temp_increase_min"]))):
+                self.ws.cell(row=self.row, column=14+i, value=round(temp_rise[0], self.rounding_factor))
+                self.datastore['Tests'][f'temperature_rise_test_{i}'] = "Pass"
+            else:
+                self.ws.cell(row=self.row, column=14+i, value=round(temp_rise[0], self.rounding_factor)).style = "fail"
+                self.datastore['Tests'][f'temperature_rise_test_{i}'] = "Fail"
+                heat_test = False
 
         self.datastore['heater_supply_voltage'] = supply_voltage
         self.datastore['heater_supply_current'] = supply_current
@@ -208,22 +240,23 @@ class LDOmeasure:
         #Turn on HV
         hv_results = {}
         self.r1.power("ON", "hvpullup")
+        hv_test = True
         for i in range(8):
             #Do the positive voltage with open termination
             self.k.set_relay(0 << i, 0)
             self.c.turn_on(i)
             print(f"{self.prefix} --> HV reached max value, waiting {float(self.json_data['hv_stability_wait'])} seconds to stabilize...")
             time.sleep(float(self.json_data['hv_stability_wait']))
-            hv_results[i] = {"P,NT,V" : self.c.get_voltage(i)}
-            hv_results[i]["P,NT,C"] = self.c.get_current(i)
+            hv_results[i] = {"pos_open_V" : self.c.get_voltage(i)}
+            hv_results[i]["pos_open_I"] = self.c.get_current(i)
 
             #Turn on 10k termination
             self.k.set_relay(0 << i, 1 << i)
             print(f"{self.prefix} --> HV termination switched, waiting {float(self.json_data['hv_termination_wait'])} seconds...")
             time.sleep(float(self.json_data['hv_termination_wait']))
 
-            hv_results[i]["P,10k,V"] = self.c.get_voltage(i)
-            hv_results[i]["P,10k,C"] = self.c.get_current(i)
+            hv_results[i]["pos_term_V"] = self.c.get_voltage(i)
+            hv_results[i]["pos_term_I"] = self.c.get_current(i)
             self.c.turn_off(i)
             print(f"{self.prefix} --> HV turned off, waiting {float(self.json_data['hv_stability_wait'])} seconds to stabilize...")
             time.sleep(float(self.json_data['hv_stability_wait']))
@@ -233,58 +266,75 @@ class LDOmeasure:
             self.c.turn_on(i+8)
             print(f"{self.prefix} --> HV reached max value, waiting {float(self.json_data['hv_stability_wait'])} seconds to stabilize...")
             time.sleep(float(self.json_data['hv_stability_wait']))
-            hv_results[i]["N,NT,V"] = self.c.get_voltage(i)
-            hv_results[i]["N,NT,C"] = self.c.get_current(i)
+            hv_results[i]["neg_open_V"] = self.c.get_voltage(i+8)
+            hv_results[i]["neg_open_I"] = self.c.get_current(i+8)
 
             #Turn on 10k termination
             self.k.set_relay(1 << i, 1 << i)
             print(f"{self.prefix} --> HV termination switched, waiting {float(self.json_data['hv_termination_wait'])} seconds...")
             time.sleep(float(self.json_data['hv_termination_wait']))
 
-            hv_results[i]["N,10k,V"] = self.c.get_voltage(i)
-            hv_results[i]["N,10k,C"] = self.c.get_current(i)
+            hv_results[i]["neg_term_V"] = self.c.get_voltage(i+8)
+            hv_results[i]["neg_term_I"] = self.c.get_current(i+8)
             self.c.turn_off(i+8)
             print(f"{self.prefix} --> HV turned off, waiting {float(self.json_data['hv_stability_wait'])} seconds to stabilize...")
             time.sleep(float(self.json_data['hv_stability_wait']))
 
             print(f"{self.prefix} --> Channel {i} HV results are {hv_results[i]}")
-            try:
-                hv_results[i]["P,NT,R"] = float(hv_results[i]["P,NT,V"])/float(hv_results[i]["P,NT,C"])
-            except ZeroDivisionError:
-                hv_results[i]["P,NT,R"] = 0
-            try:
-                hv_results[i]["P,10k,R"] = float(hv_results[i]["P,10k,V"])/float(hv_results[i]["P,10k,C"])
-            except ZeroDivisionError:
-                hv_results[i]["P,10k,R"] = 0
-            try:
-                hv_results[i]["N,NT,R"] = float(hv_results[i]["N,NT,V"])/float(hv_results[i]["N,NT,C"])
-            except ZeroDivisionError:
-                hv_results[i]["N,NT,R"] = 0
-            try:
-                hv_results[i]["N,10k,R"] = float(hv_results[i]["N,NT,V"])/float(hv_results[i]["N,10k,C"])
-            except ZeroDivisionError:
-                hv_results[i]["N,10k,R"] = 0
-            self.ws.cell(row=self.row, column=12+(i*4), value=round(float(hv_results[i]["P,NT,R"]), self.rounding_factor))
-            self.ws.cell(row=self.row, column=13+(i*4), value=round(float(hv_results[i]["P,10k,R"]), self.rounding_factor))
-            self.ws.cell(row=self.row, column=14+(i*4), value=round(float(hv_results[i]["N,NT,R"]), self.rounding_factor))
-            self.ws.cell(row=self.row, column=15+(i*4), value=round(float(hv_results[i]["N,10k,R"]), self.rounding_factor))
 
-            self.datastore[f'hv_ch{i}'] = {'pos_open_V' : hv_results[i]["P,NT,V"]}
-            self.datastore[f'hv_ch{i}']['pos_open_V'] = hv_results[i]["P,NT,V"]
-            self.datastore[f'hv_ch{i}']['pos_open_I'] = hv_results[i]["P,NT,C"]
-            self.datastore[f'hv_ch{i}']['pos_open_R'] = hv_results[i]["P,NT,R"]
-            self.datastore[f'hv_ch{i}']['pos_term_V'] = hv_results[i]["P,10k,V"]
-            self.datastore[f'hv_ch{i}']['pos_term_I'] = hv_results[i]["P,10k,C"]
-            self.datastore[f'hv_ch{i}']['pos_term_R'] = hv_results[i]["P,10k,R"]
-            self.datastore[f'hv_ch{i}']['neg_open_V'] = hv_results[i]["N,NT,V"]
-            self.datastore[f'hv_ch{i}']['neg_open_I'] = hv_results[i]["N,NT,C"]
-            self.datastore[f'hv_ch{i}']['neg_open_R'] = hv_results[i]["N,NT,R"]
-            self.datastore[f'hv_ch{i}']['neg_term_V'] = hv_results[i]["N,10k,V"]
-            self.datastore[f'hv_ch{i}']['neg_term_I'] = hv_results[i]["N,10k,C"]
-            self.datastore[f'hv_ch{i}']['neg_term_R'] = hv_results[i]["N,10k,R"]
+            try:
+                hv_results[i]["pos_open_R"] = float(hv_results[i]["pos_open_V"])/float(hv_results[i]["pos_open_I"])
+            except ZeroDivisionError:
+                hv_results[i]["pos_open_R"] = 0
+            try:
+                hv_results[i]["pos_term_R"] = float(hv_results[i]["pos_term_V"])/float(hv_results[i]["pos_term_I"])
+            except ZeroDivisionError:
+                hv_results[i]["pos_term_R"] = 0
+            try:
+                hv_results[i]["neg_open_R"] = float(hv_results[i]["neg_open_V"])/float(hv_results[i]["neg_open_I"])
+            except ZeroDivisionError:
+                hv_results[i]["neg_open_R"] = 0
+            try:
+                hv_results[i]["neg_term_R"] = float(hv_results[i]["neg_term_V"])/float(hv_results[i]["neg_term_I"])
+            except ZeroDivisionError:
+                hv_results[i]["neg_term_R"] = 0
 
-        self.wb.save(self.path_to_spreadsheet)
+            for num,j in enumerate(["pos_open_R", "pos_term_R", "neg_open_R", "neg_term_R"]):
+                if ((float(hv_results[i][j]) < float(self.json_data["hv_resistance_max"])) and (float(hv_results[i][j]) > float(self.json_data["hv_resistance_min"]))):
+                    self.ws.cell(row=self.row, column=18+(i*4)+num, value=round(float(hv_results[i][j]), self.rounding_factor))
+                    self.datastore['Tests'][f'hv_test_ch{i}_{j}'] = "Pass"
+                else:
+                    self.ws.cell(row=self.row, column=18+(i*4)+num, value=round(float(hv_results[i][j]), self.rounding_factor)).style = "fail"
+                    self.datastore['Tests'][f'hv_test_ch{i}_{j}'] = "Fail"
+                    hv_test = False
+
+            self.ws.cell(row=self.row, column=12+(i*4), value=round(float(hv_results[i]["pos_open_R"]), self.rounding_factor))
+            self.ws.cell(row=self.row, column=13+(i*4), value=round(float(hv_results[i]["pos_term_R"]), self.rounding_factor))
+            self.ws.cell(row=self.row, column=14+(i*4), value=round(float(hv_results[i]["neg_open_R"]), self.rounding_factor))
+            self.ws.cell(row=self.row, column=15+(i*4), value=round(float(hv_results[i]["neg_term_R"]), self.rounding_factor))
+
+            self.datastore[f'hv_ch{i}'] = {'pos_open_V' : hv_results[i]["pos_open_V"]}
+            self.datastore[f'hv_ch{i}']['pos_open_I'] = hv_results[i]["pos_open_I"]
+            self.datastore[f'hv_ch{i}']['pos_open_R'] = hv_results[i]["pos_open_R"]
+            self.datastore[f'hv_ch{i}']['pos_term_V'] = hv_results[i]["pos_term_V"]
+            self.datastore[f'hv_ch{i}']['pos_term_I'] = hv_results[i]["pos_term_I"]
+            self.datastore[f'hv_ch{i}']['pos_term_R'] = hv_results[i]["pos_term_R"]
+            self.datastore[f'hv_ch{i}']['neg_open_V'] = hv_results[i]["neg_open_V"]
+            self.datastore[f'hv_ch{i}']['neg_open_I'] = hv_results[i]["neg_open_I"]
+            self.datastore[f'hv_ch{i}']['neg_open_R'] = hv_results[i]["neg_open_R"]
+            self.datastore[f'hv_ch{i}']['neg_term_V'] = hv_results[i]["neg_term_V"]
+            self.datastore[f'hv_ch{i}']['neg_term_I'] = hv_results[i]["neg_term_I"]
+            self.datastore[f'hv_ch{i}']['neg_term_R'] = hv_results[i]["neg_term_R"]
+
         self.r1.power("OFF", "hvpullup")
+
+        if (fan_test and heat_test and hv_test):
+            self.ws.cell(row=self.row, column=1, value=self.test_name)
+            self.datastore['overall'] = "Pass"
+        else:
+            self.ws.cell(row=self.row, column=1, value=self.test_name).style = "fail"
+            self.datastore['overall'] = "Fail"
+        self.wb.save(self.path_to_spreadsheet)
 
         end_time = datetime.now()
         test_time = end_time - self.start_time
